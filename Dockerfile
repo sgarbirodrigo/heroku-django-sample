b93286ce9b8e4f429e556d4af4144701df4364dd
RodrigoS <sgarbi.rodrigo@gmail.com>
2023-04-03 20:55:31 -0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,13 @@ WORKDIR /code
 ADD requirements.txt /code/
 RUN pip install -r requirements.txt
 COPY . /code/
+
+
+# collect static files
+RUN python manage.py collectstatic --noinput
+
+# add and run as non-root user
+RUN adduser -D myuser
+USER myuser
+# run gunicorn
+CMD gunicorn hello_django.wsgi:application --bind 0.0.0.0:$PORT
--- bca/settings.py
+++ bca/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # new
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -131,7 +132,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
